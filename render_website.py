9b40dbfdd8f4087decee7e3a0d09b0d276b5bc18
Максим Калинин <maksim.kalinin1@gmail.com>
2022-11-10 13:06:13 +0300

    Split books list by pages.

--- render_website.py
+++ render_website.py
@@ -3,6 +3,7 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server, shell
 from more_itertools import chunked
+from pathlib import Path
 
 
 
@@ -15,17 +16,37 @@ def on_reload():
     
     template = env.get_template('template.html')
     
-    rendered_page = template.render(books=books)
+    for number_page, books_on_pages in enumerate(books_2, 1):
+        books_on_pages = list(chunked(books_on_pages, 2))
+        rendered_page = template.render(books_on_pages=books_on_pages)
     
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+        with open(Path.cwd() / 'pages' / f'index{number_page}.html', 'w', encoding="utf8") as file:
+            file.write(rendered_page)
+
+
+
+
+    #rendered_page = template.render(books=books)
+    
+    #with open('index.html', 'w', encoding="utf8") as file:
+        #file.write(rendered_page)
     
 
+def create_dirs(*paths):
+    for path in paths:
+        Path(path).mkdir(parents=True, exist_ok=True)
+
+
 if __name__ == '__main__':
+
+    path_to_pages = Path.cwd() / 'pages'
+    create_dirs(path_to_pages)
+
     with open("books.json", "r") as file:
         books_json = file.read()
     
-    books = list(chunked(json.loads(books_json), 2))
+    #books = list(chunked(json.loads(books_json), 2))
+    books_2 = list(chunked(json.loads(books_json), 20))
         
     on_reload()
 
