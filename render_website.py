379f8c814528b4d31db4567552c3a6a4b2438697
Максим Калинин <maksim.kalinin1@gmail.com>
2022-11-21 15:47:43 +0300

    cleaned up the code

--- render_website.py
+++ render_website.py
@@ -19,16 +19,17 @@ def on_reload():
 
     folder_books = Path(Path.cwd() / 'books')
     files_in_folder_books = len(list(folder_books.iterdir()))
-    pages_count =  math.ceil(files_in_folder_books / 20)
+    pages_count =  math.ceil(files_in_folder_books / books_on_page)
+    column_amount = 2
 
     for number_page, books_on_pages in enumerate(books, 1):
         if number_page == 1:
-            pagination_pages = [i for i in range(1, 4)]
+            pagination_pages = [i for i in range(number_page, number_page + 3)]
         elif number_page == pages_count:
             pagination_pages = [i for i in range(pages_count - 2, pages_count + 1)]
         else:
             pagination_pages = [i for i in range(number_page - 1, number_page + 2)]
-        books_on_pages = list(chunked(books_on_pages, 2))
+        books_on_pages = list(chunked(books_on_pages, column_amount))
         rendered_page = template.render(
             books_on_pages=books_on_pages,
             number_page=number_page,
@@ -51,9 +52,10 @@ if __name__ == '__main__':
     create_dirs(path_to_pages)
 
     with open("books.json", "r") as file:
-        books_json = file.read()
+        books_json = json.load(file)
 
-    books = list(chunked(json.loads(books_json), 20))
+    books_on_page = 20
+    books = list(chunked(books_json, books_on_page))
         
     on_reload()
 
